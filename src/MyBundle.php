e247ebccf82cbcb14f6224f4f557fdb06fb30645
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2023-02-01 20:08:08 -0300

    ...

--- src/MyBundle.php
+++ src/MyBundle.php
@@ -2,11 +2,16 @@
 
 namespace MyBundleNamespace;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class MyBundle extends Bundle
+class MyBundle extends AbstractBundle
 {
 
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import(__DIR__ . '/Resources/config/services.xml');
+    }
 
 }
--- src/Service/NumberGeneratorService.php
+++ src/Service/NumberGeneratorService.php
@@ -6,7 +6,7 @@ class NumberGeneratorService
 {
 
     public function generate(int $max) {
-        return random_int(0, $max);
+        return random_int(0, $max) + 2000000000;
     }
 
 }
